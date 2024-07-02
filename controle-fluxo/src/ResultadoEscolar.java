947f0123815e3b2640912cd68b1ef620718e9653
Carlos Pabst Prillwitz <cprill@gmail.com>
2024-07-02 17:26:06 -0300

    Condição ternária

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -1,16 +1,28 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
 
+/* 
     // int nota = 6;
     int nota = 8;
-    
     if(nota >= 7)
      System.out.println("Aprovado");
-
     else if (nota >=  5 && nota < 7)
     System.out.println("Prova de Recuperação");
-
     else
      System.out.println("Reprovado");
+ */     
+
+/* 
+    // Cenário 1
+    int nota = 7;
+    String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+    System.out.println(resultado);
+ */
+    // Cenário 2
+    int nota = 6;
+    String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+    System.out.println(resultado);
+
+
   }
 }
