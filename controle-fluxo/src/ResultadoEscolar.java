402e5edc67833536153cc0a82a9aef60beeb8b30
Carlos Pabst Prillwitz <cprill@gmail.com>
2024-07-02 17:18:42 -0300

    Condicionais encadeadas

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -7,6 +7,9 @@ public class ResultadoEscolar {
     if(nota >= 7)
      System.out.println("Aprovado");
 
+    else if (nota >=  5 && nota < 7)
+    System.out.println("Prova de Recuperação");
+
     else
      System.out.println("Reprovado");
   }
