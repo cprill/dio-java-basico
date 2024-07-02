1276bb93b8656b9e15c6bad4a5594699b53e09fa
Carlos Pabst Prillwitz <cprill@gmail.com>
2024-07-02 17:12:20 -0300

    Condicionais Composta

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -4,10 +4,12 @@ public class CaixaEletronico {
     // double valorSolicitado = 17.0;
     double valorSolicitado = 26.0;
 
-    if(valorSolicitado < saldo)
+    if(valorSolicitado < saldo){
      saldo = saldo - valorSolicitado;
-
-     System.out.println(saldo);
+     System.out.println("Novo saldo: " + saldo);
+    } else {
+      System.out.println("Saldo insuficiente");
+    }
   }
 
 }
