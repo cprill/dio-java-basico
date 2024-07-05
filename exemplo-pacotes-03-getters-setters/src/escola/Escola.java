cadab72770f7d70cb104f6afc5a9424c8aa3ab13
Carlos Pabst Prillwitz <cprill@gmail.com>
2024-07-05 18:06:32 -0300

    Exercicio Getters e Setters - finalizado

--- exemplo-pacotes-03-getters-setters/src/escola/Escola.java
+++ exemplo-pacotes-03-getters-setters/src/escola/Escola.java
@@ -3,11 +3,14 @@ package escola;
 public class Escola {
   public static void main(String[] args) {
 		Aluno felipe = new Aluno();
-		felipe.nome="Felipe";
-		felipe.idade = 8;
+		// felipe.nome="Felipe";
+		// felipe.idade = 8;
+    felipe.setNome("Felipe");
+		felipe.setIdade(8);
 		
-		System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
-		//RESULTADO NO CONSOLE
+		// System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
+    System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");	
+    //RESULTADO NO CONSOLE
 		//O aluno Felipe tem 8 anos 		
 	}
 }
